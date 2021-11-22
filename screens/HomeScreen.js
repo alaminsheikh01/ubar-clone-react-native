17f541481280c29289517a685d1d5c86891ab628
alamin <alamin@gmail.com>
2021-11-22 16:13:24 +0600

    badly finished for require google console biling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,11 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const HomeScreen = () => {
+  // console.log(GOOGLE_MAPS_APIKEY);
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -11,6 +14,30 @@ const HomeScreen = () => {
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
         />
+
+        <GooglePlacesAutocomplete
+          placeholder="Where Form ?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          onPress={(data, details = null) => {
+            console.log(data);
+            console.log(details);
+          }}
+          fetchDetails={true}
+          query={{
+            key: "AIzaSyBKdi44_mLdjJChSlB7kLFjM-xOkHakue4",
+            language: "en",
+          }}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
